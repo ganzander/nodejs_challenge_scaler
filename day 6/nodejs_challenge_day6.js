36c76413b97a773a7e3182bd6929d1e7cf95493f
ganzander <ganeshmangla2003@gmail.com>
2024-02-07 13:32:04 +0530

    Day 6 completed

---
+++ day 6/nodejs_challenge_day6.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/greet", (req, res) => {
+  const name = req.query.name;
+  if (name) {
+    res.send(`Hello, ${name}!`);
+  } else {
+    res.send("Hello, Guest!");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
