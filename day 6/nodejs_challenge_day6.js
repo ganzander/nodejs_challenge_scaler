9e8dc4185d6e04bad68a582e8e0212c44fcee2a8
ganzander <ganeshmangla2003@gmail.com>
2024-02-07 13:42:19 +0530

    Day 6 changed

--- day 6/nodejs_challenge_day6.js
+++ day 6/nodejs_challenge_day6.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get("/greet", (req, res) => {
+function greetHandler(req, res) {
   const name = req.query.name;
   if (name) {
     res.send(`Hello, ${name}!`);
   } else {
     res.send("Hello, Guest!");
   }
+}
+app.get("/greet", (req, res) => {
+  greetHandler(req, res);
 });
 
 app.listen(port, () => {
