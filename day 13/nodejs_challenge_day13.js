1f6e74de51ab2bb5869d7a5d72ef8c82c09d6d6c
ganzander <ganeshmangla2003@gmail.com>
2024-02-13 18:09:51 +0530

    Day 13 completed

---
+++ day 13/nodejs_challenge_day13.js
@@ -0,0 +1,33 @@
+const WebSocket = require("ws");
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+function setupWebSocket(server) {
+  const wss = new WebSocket.Server({ server });
+  wss.on("connection", function connection(ws) {
+    console.log("WebSocket client connected");
+    ws.on("message", (message) => {
+      console.log("Received message:", message);
+      ws.send(message);
+    });
+    ws.on("close", () => {
+      console.log("WebSocket client disconnected");
+    });
+  });
+
+  const app = express();
+
+  app.get("/websocket", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+  server.on("request", app);
+}
+
+const server = http.createServer();
+setupWebSocket(server);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
