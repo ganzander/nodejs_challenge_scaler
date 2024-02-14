13a0b1227f38229b44a559500b1e179cdf4b5c29
ganzander <ganeshmangla2003@gmail.com>
2024-02-14 14:13:07 +0530

    Day 14 completed

---
+++ day 14/nodejs_challenge_day14.js
@@ -0,0 +1,35 @@
+const cache = {};
+const express = require("express");
+const app = express();
+
+function cachingMiddleware(req, res, next) {
+  const { url } = req;
+  const cachedResponse = cache[url];
+  if (cachedResponse) {
+    const { response, expirationTime } = cachedResponse;
+    if (expirationTime > Date.now()) {
+      res.send(response);
+      return;
+    } else {
+      delete cache[url];
+    }
+  }
+  const originalSend = res.send;
+  res.send = function (body) {
+    cache[url] = {
+      response: body,
+      expirationTime: Date.now() + 5 * 60 * 1000,
+    };
+    originalSend.call(this, body);
+  };
+  next();
+}
+
+app.use(cachingMiddleware);
+app.get("/example", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
