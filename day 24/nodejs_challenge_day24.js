fb3a8afd8057106d4e0273cb117375abb11bda3a
ganzander <ganeshmangla2003@gmail.com>
2024-02-24 10:16:40 +0530

    Day 24 completed

---
+++ day 24/nodejs_challenge_day24.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+const uri = process.env.mongoURL;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.post("/products", async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    const product = new Product({
+      name,
+      description,
+      price,
+    });
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price } = req.body;
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { name, description, price },
+      { new: true }
+    );
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port 3000`);
+});
